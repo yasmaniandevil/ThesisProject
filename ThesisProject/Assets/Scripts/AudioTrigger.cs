77b71879c4b783eacc787d60afbd5153374e00e5
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-12-03 16:13:00 -0500

    redoing the banks

---
+++ ThesisProject/Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AkSoundEngine.PostEvent("PortalEnter", gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        AkSoundEngine.PostEvent("PortalExit", gameObject);
+        Debug.Log("exit");
+    }
+}
